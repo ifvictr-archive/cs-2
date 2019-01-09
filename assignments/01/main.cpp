cbaee6595a2c1fdf942fed89e3fd0c7fdf83936d
Victor Truong <me@ifvictr.com>
2019-01-09 12:06:34 -0800

    Adhere to style guide

--- assignments/01/main.cpp
+++ assignments/01/main.cpp
@@ -1,23 +1,49 @@
+/*
+ * Author      : Victor Truong
+ * CS 2        : MTWR, 7:30a – 12:30p
+ * Assignment 1: Madlibs
+ * Due date    : 1/8/19
+ */
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+/*
+ * Madlibs
+ * 
+ * This program takes user input to generate a Madlib story.
+ * 
+ * INPUT:
+ *   animal   : Type of animal
+ *   food     : Type of food
+ *   material1: Type of building material
+ *   material2: Type of building material
+ *   material3: Type of building material
+ *   pastVerb1: Past verb 1
+ *   pastVerb2: Past verb 2
+ *   pastVerb3: Past verb 3
+ *   noun1    : Noun 1
+ *   noun2    : Noun 2
+ *
+ * OUTPUT:
+ *   story: Resulting story from user inputs
+ */
 int main() {
-    string animal;
-    string food;
-    string material1;
-    string material2;
-    string material3;
-    string pastVerb1;
-    string pastVerb2;
-    string pastVerb3;
-    string noun1;
-    string noun2;
+    string animal;    // INPUT: Type of animal
+    string food;      // INPUT: Type of food
+    string material1; // INPUT: Type of building material
+    string material2; // INPUT: Type of building material
+    string material3; // INPUT: Type of building material
+    string pastVerb1; // INPUT: Past verb 1
+    string pastVerb2; // INPUT: Past verb 2
+    string pastVerb3; // INPUT: Past verb 3
+    string noun1;     // INPUT: Noun 1
+    string noun2;     // INPUT: Noun 2
 
     string measuringStick(80, '-');
 
-    // Prompt for input
+    // INPUT: Prompt user for words
     cout << "Please enter a type of animal: ";
     cin >> animal;
     cout << "\nPlease enter a type of food: ";
@@ -40,22 +66,25 @@ int main() {
     cin >> noun2;
     cout << "\n";
 
-    // Output madlib result
+    // OUTPUT: Generated story from user input
     cout << measuringStick << "\n";
     cout <<
-        "Once upon a time there was an old mother " << animal << " who had three little\n"
-        << animal << "s. She did not have enough " << food << " to feed them. So when they were\n"
-        "old enough, she sent them out into the world to seek their fortunes.\n\n";
+        "Once upon a time there was an old mother " << animal << " who had "
+        "three little\n" << animal << "s. She did not have enough " << food
+        << " to feed them. So when they were\nold enough, she sent them out "
+        "into the world to seek their fortunes.\n\n";
     cout <<
-        "The first little " << animal << " was very lazy. He didn't want to work at all and he\n"
-        "built his house out of " << material1 << ". The second little " << animal << " worked a little\n"
-        "bit harder but he was somewhat lazy too and he built his house out of\n"
-        << material2 << ". Then, they " << pastVerb1 << " and " << pastVerb2 << " and " << pastVerb3 << " together the\n"
-        "rest of the day.\n\n";
+        "The first little " << animal << " was very lazy. He didn't want to "
+        "work at all and he\nbuilt his house out of " << material1 << ". The "
+        "second little " << animal << " worked a little\nbit harder but he was "
+        "somewhat lazy too and he built his house out of\n" << material2
+        << ". Then, they " << pastVerb1 << " and " << pastVerb2 << " and "
+        << pastVerb3 << " together the\nrest of the day.\n\n";
     cout <<
-        "The third little " << animal << " worked hard all day and built his house with \n"
-        << material3 << ". It was a sturdy house complete with a fine " << noun1 << " and\n"
-        << noun2 << ". It looked like it could withstand the strongest winds.\n";
+        "The third little " << animal << " worked hard all day and built his"
+        "house with \n" << material3 << ". It was a sturdy house complete with "
+        " a fine " << noun1 << " and\n" << noun2 << ". It looked like it could "
+        " withstand the strongest winds.\n";
     cout << measuringStick << "\n";
 
     return 0;
--- labs/02/lab02.cpp
+++ labs/02/lab02.cpp
@@ -1,19 +1,50 @@
+/*
+ * Author  : Victor Truong
+ * CS 2    : MTWR, 7:30a – 12:30p
+ * Lab 2   : Arithmetic Operations and Data Types
+ * Due date: 1/8/19
+ */
 #include <cmath>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+/*
+ * ISBN Checksum / Letter Position Calculator
+ *
+ * This program calculates the checksum of a given ISBN and also the position
+ * of a given letter in the alphabet chart.
+ * 
+ * INPUT:
+ *   ex  : Exercise number to execute
+ *   isbn: ISBN to calculate checksum
+ *
+ * OUTPUT:
+ *   checksum: Checksum of ISBN
+ *   position: Position of the letter in the alphabet
+ */
 int main() {
-    int ex;
+    cout <<
+        "*****************************************\n"
+        "  Author: Victor Truong\n"
+        "  ID    : 10180511\n"
+        "  CS 2  : MTWR, 7:30a – 12:30p\n"
+        "  Lab 2 : Arithmetic Operations and Data Types\n"
+        "*****************************************\n";
+
+    int ex; // INPUT: Exercise number to execute
+
     cout << "Which exercise? ";
     cin >> ex;
     cout << endl;
+
     if (ex == 1) {
         // All Exercise 1 code
-        int isbn;
+        int isbn; // INPUT: ISBN to calculate checksum
         cout << "Please enter the first 9 digits of the ISBN: ";
         cin >> isbn;
+
         int digit9 = isbn % 10;
         isbn /= 10;
         int digit8 = isbn % 10;
@@ -32,16 +63,27 @@ int main() {
         isbn /= 10;
         int digit1 = isbn % 10;
 
-        int checksum = (digit1 + (digit2 * 2) + (digit3 * 3) + (digit4 * 4) + (digit5 * 5) + (digit6 * 6) + (digit7 * 7) + (digit8 * 8) + (digit9 * 9)) % 11;
+        int checksum = (digit1 +
+            (digit2 * 2) +
+            (digit3 * 3) +
+            (digit4 * 4) +
+            (digit5 * 5) +
+            (digit6 * 6) +
+            (digit7 * 7) +
+            (digit8 * 8) +
+            (digit9 * 9)
+        ) % 11; // OUTPUT: Checksum of ISBN
 
         cout << "\nChecksum: " << checksum << "\n";
     }
     else if (ex == 2) {
         // All Exercise 2 code
-        char letter;
-        int position;
+        char letter;  // INPUT: Letter to calculate position of
+        int position; // OUTPUT: Position of the letter in the alphabet
+
         cout << "Enter a character: ";
         cin >> letter;
+
         position = 26 - ('z' - letter);
         cout << "\n" << letter << " is letter " << position << "\n";
     }
