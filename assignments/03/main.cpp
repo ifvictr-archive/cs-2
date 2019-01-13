b52a81b6884cbc60b7e22146af93bca1352f7897
Victor Truong <me@ifvictr.com>
2019-01-12 19:40:44 -0800

    Fix assignment 3

--- assignments/03/main.cpp
+++ assignments/03/main.cpp
@@ -61,62 +61,65 @@ int main() {
                 cin >> num3;
             }
         }
+        cout << "\n";
     }
     else {
-        cout << "Error: Operation not supported.\n";
+        cout << "\nError: Operation not supported.\n";
     }
     // OUTPUT: Generated story from user input
-    cout << "\n";
     if (operation == "addition") {
-        cout << "Equation: " << num1 << " + " << num2 << "\n";
-        cout << "Result: " << (num1 + num2) << "\n";
+        cout << "Equation: " << num1 << " + " << num2 << "\n\n";
+        cout << "Results: " << (num1 + num2) << "\n";
     }
     else if (operation == "division") {
-        cout << "Equation: " << num1 << " / " << num2 << "\n";
+        cout << "Equation: " << num1 << " / " << num2 << "\n\n";
         if (!num2) {
             cout << "Error: Cannot divide by zero.\n";
         }
         else {
-            cout << "Result: " << (num1 / num2) << "\n";
+            cout << "Results: " << (num1 / num2) << "\n";
         }
     }
     else if (operation == "multiplication") {
-        cout << "Equation: " << num1 << " * " << num2 << "\n";
-        cout << "Result: " << (num1 * num2) << "\n";
+        cout << "Equation: " << num1 << " * " << num2 << "\n\n";
+        cout << "Results: " << (num1 * num2) << "\n";
     }
     else if (operation == "subtraction") {
-        cout << "Equation: " << num1 << " - " << num2 << "\n";
-        cout << "Result: " << (num1 - num2) << "\n";
+        cout << "Equation: " << num1 << " - " << num2 << "\n\n";
+        cout << "Results: " << (num1 - num2) << "\n";
     }
     else if (operation == "fabs") {
-        cout << "Equation: fabs(" << num1 << ")\n";
-        cout << "Result: " << fabs(num1) << "\n";
+        cout << "Equation: fabs(" << num1 << ") \n\n";
+        cout << "Results: " << fabs(num1) << "\n";
     }
     else if (operation == "pythagorean") {
-        cout << "Equation: sqrt(" << num1 << "^2 + " << num2 << "^2)\n";
-        cout << "Result: " << sqrt(pow(num1, 2) + pow(num2, 2)) << "\n";
+        cout << "Equation: sqrt(" << num1 << "^2 + " << num2 << "^2)\n\n";
+        cout << "Results: " << sqrt(pow(num1, 2) + pow(num2, 2)) << "\n";
     }
     else if (operation == "quadratic") {
         cout << "Equation: " << num1 << "x^2 + " << num2 << "x + " << num3
-            << " = 0\n";
+            << " = 0\n\n";
         float discriminant = pow(num2, 2) - (4 * num1 * num3);
-        if (discriminant < 0) {
+        if (num1 == 0) {
+            cout << "Error: Cannot divide by zero.\n";
+        }
+        else if (discriminant < 0) {
             cout << "Error: Cannot take square root of a negative number.\n";
         }
         else {
             float answer1 = (-1 * num2 + sqrt(discriminant)) / (2 * num1);
             float answer2 = (-1 * num2 - sqrt(discriminant)) / (2 * num1);
-            cout << "Result: " << answer1 << ", " << answer2 << "\n";
+            cout << "Results: " << answer1 << ", " << answer2 << "\n";
         }
 
     }
     else if (operation == "sqrt") {
-        cout << "Equation: sqrt(" << num1 << ")\n";
+        cout << "Equation: sqrt(" << num1 << ")\n\n";
         if (num1 < 0) {
             cout << "Error: Cannot take square root of a negative number.\n";
         }
         else {
-            cout << "Result: " << sqrt(num1) << "\n";
+            cout << "Results: " << sqrt(num1) << "\n";
         }
     }
 
